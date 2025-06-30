8b5d562014f0a02cf768b9e0421612a824220deb
pagusupu <kuranphos@gmail.com>
2025-07-01 10:51:13 +1200

    what about now

--- hosts/_common/programs/git.nix
+++ hosts/_common/programs/git.nix
@@ -2,10 +2,8 @@ _: {
   programs.git = {
     enable = true;
     config = {
-      github.user = "pagusupu";
       init.defaultBranch = "main";
       user = {
-        email = "me@pagu.cafe";
         name = "pagusupu";
         signingKey = "/home/pagu/.ssh/id_ed25519.pub";
       };
--- hosts/_common/programs/shell/yazi.nix
+++ hosts/_common/programs/shell/yazi.nix
@@ -2,7 +2,7 @@
   home-manager.users.pagu = {
     programs.yazi = {
       enable = true;
-      settings.manager = {
+      settings.mgr = {
         show_hidden = true;
         sort_by = "natural";
         sort_dir_first = true;
