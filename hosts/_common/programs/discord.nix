8da57d7d1f6fc3968feed3d0f1eef7aada7423e4
pagusupu <kuranphos@gmail.com>
2025-09-08 11:02:06 +1200

    discord & yazi

--- hosts/_common/programs/discord.nix
+++ hosts/_common/programs/discord.nix
@@ -6,6 +6,10 @@
       vesktop.enable = true;
       config = {
         plugins = {
+          accountPanelServerProfile = {
+            enable = true;
+            prioritizeServerProfile = true;
+          };
           alwaysTrust = {
             enable = true;
             file = true;
@@ -14,38 +18,72 @@
             enable = true;
             anonymiseByDefault = true;
           };
+          betterNotesBox = {
+            enable = true;
+            noSpellCheck = true;
+          };
           callTimer = {
             enable = true;
             format = "human";
+          };
+          ignoreActivities = {
+            enable = true;
+            ignoreCompeting = true;
+            ignorePlaying = true;
+            ignoreStreaming = true;
           };
           newGuildSettings = {
             enable = true;
             messages = "only@Mentions";
             everyone = false;
             role = false;
+            showAllChannels = false;
+          };
+          noBlockedMessages = {
+            enable = true;
+            ignoreBlockedMessages = true;
           };
           notificationVolume = {
             enable = true;
             notificationVolume = 50.0;
           };
+          openInApp = {
+            enable = true;
+            epic = false;
+            itunes = false;
+            spotify = false;
+            tidal = false;
+          };
+          typingTweaks = {
+            enable = true;
+            showRoleColors = false;
+          };
           alwaysAnimate.enable = true;
+          betterGifAltText.enable = true;
+          betterGifPicker.enable = true;
+          # betterSettings.enable = true;
           betterUploadButton.enable = true;
           clearURLs.enable = true;
           disableCallIdle.enable = true;
+          dontRoundMyTimestamps.enable = true;
           favoriteEmojiFirst.enable = true;
           favoriteGifSearch.enable = true;
           fakeNitro.enable = true;
           fixImagesQuality.enable = true;
           fixSpotifyEmbeds.enable = true;
           fixYoutubeEmbeds.enable = true;
+          friendsSince.enable = true;
           gameActivityToggle.enable = true;
           loadingQuotes.enable = true;
+          messageClickActions.enable = true;
           noF1.enable = true;
           noOnboardingDelay.enable = true;
           noProfileThemes.enable = true;
           onePingPerDM.enable = true;
           plainFolderIcon.enable = true;
+          silentMessageToggle.enable = true;
           stickerPaste.enable = true;
+          unindent.enable = true;
           userMessagesPronouns.enable = true;
           voiceChatDoubleClick.enable = true;
           webRichPresence.enable = true;
@@ -56,7 +94,7 @@
         useQuickCss = true;
       };
       quickCss = ''
-        @import url("https://croissantdunord.github.io/discord-adblock/adblock.css");
+        @import url(https://croissantdunord.github.io/discord-adblock/adblock.css);
         body { --font: ""; }
       '';
       extraConfig.SKIP_HOST_UPDATE = true;
--- hosts/_common/programs/shell/yazi.nix
+++ hosts/_common/programs/shell/yazi.nix
@@ -2,18 +2,54 @@
   home-manager.users.pagu = {
     programs.yazi = {
       enable = true;
-      settings.mgr = {
-        show_hidden = true;
-        sort_by = "natural";
-        sort_dir_first = true;
-      };
       plugins = {
         inherit
           (pkgs.yaziPlugins)
           full-border
+          mediainfo
           no-status
+          ouch
+          rich-preview
           starship
           ;
+      };
+      settings = {
+        mgr = {
+          show_hidden = true;
+          sort_by = "natural";
+          sort_dir_first = true;
+        };
+        plugin = let
+          ouch = type: {
+            mime = "application/${type}";
+            run = "ouch";
+          };
+          rich = type: {
+            name = "*.${type}";
+            run = "rich-preview";
+          };
+          media = type: {
+            mime = type;
+            run = "mediainfo";
+          };
+        in {
+          prepend_previewers = [
+            (ouch "*zip")
+            (ouch "x-tar")
+            (ouch "x-rar")
+            (ouch "x-7z-compressed")
+
+            (rich "md")
+            (rich "json")
+
+            (media "{audio,video,image}/*")
+            (media "application/subrip")
+          ];
+          prepend_preloaders = [
+            (media "{audio,video,image}/*")
+            (media "application/subrip")
+          ];
+        };
       };
       initLua = ''
         require("full-border"):setup()
@@ -21,5 +57,10 @@
         require("starship"):setup()
       '';
     };
+    home.packages = with pkgs; [
+      imagemagick
+      mediainfo
+      rich-cli
+    ];
   };
 }
